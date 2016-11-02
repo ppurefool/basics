a680afff399bb5494787028c4447f9d7c61d94cf
Kwangsik You <ppurefool@hotmail.com>
2016-11-02 16:07:46 +0900

    Grid Data Select 공통함수 작성

--- authorizationserver/src/main/resources/static/js/authorizationserver/user.js
+++ authorizationserver/src/main/resources/static/js/authorizationserver/user.js
@@ -24,7 +24,7 @@ var ScreenEvent = {
 
     clickAdditionButton: function() {
 
-        GridSetting["firstGrid"].addRow({__selected__: true, "email": null, "name": null});
+        GridSetting["firstGrid"].addRow({__selected__: true});
     },
 
     clickSavingButton: function() {
