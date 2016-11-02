f7e78dd94d9f3f8fa81afc9ee8f5377b76b5463b
Kwangsik You <ppurefool@hotmail.com>
2016-11-02 13:12:41 +0900

    Initial commit

---
+++ src/main/java/io/kwangsik/BasicsApplication.java
@@ -0,0 +1,12 @@
+package io.kwangsik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicsApplication.class, args);
+	}
+}
